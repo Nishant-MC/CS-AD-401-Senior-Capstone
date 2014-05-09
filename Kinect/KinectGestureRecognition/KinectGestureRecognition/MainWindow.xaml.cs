95fafa83da5b269dbb0874fedfbabeac8951ee13
Nishant Mohanchandra <nm1345@nyu.edu>
2014-05-09 17:01:14 +0400

    Hello World - C# Kinect Style
    Kinect Sensor initialization

---
+++ Kinect/KinectGestureRecognition/KinectGestureRecognition/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * "Gesture Recognition With The Microsoft Kinect"
+ * -> Senior Capstone Project [Computer Science]
+ * 
+ * By: Nishant Mohanchandra
+ * Project Supervisor: Jay Chen
+*/
+
+// Critical System includes
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+// Including the Kinect NuiAPI
+using Microsoft.Kinect;
+
+/*
+
+ * * * Extra includes for later if needed * * *
+ 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+  
+*/
+
+namespace KinectGestureRecognition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        KinectSensor kinectSensor;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Listen to any status change for my Kinect(s)
+                KinectSensor.KinectSensors.StatusChanged += Kinects_StatusChanged;
+
+                // In case there is more than 1 Kinect, loop through all of them
+                foreach (KinectSensor kinect in KinectSensor.KinectSensors)
+                {
+                    if (kinect.Status == KinectStatus.Connected)
+                    {
+                        kinectSensor = kinect;
+                        break;
+                    }
+                }
+
+                if (KinectSensor.KinectSensors.Count == 0)
+                {
+                    MessageBox.Show("No Kinect found, sorry!");
+                }
+
+                else
+                {
+                    Initialize(); // Initialize the current Kinect sensor!
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void Kinects_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Connected:
+                    if (kinectSensor == null)
+                    {
+                        kinectSensor = e.Sensor;
+                        Initialize();
+                    }
+                    break;
+
+                case KinectStatus.Disconnected:
+                    if (kinectSensor == null)
+                    {
+                        Clean();
+                        MessageBox.Show("The Kinect was dis-kinect-ed! (Geddit? :P)");
+                    }
+                    break;
+
+                case KinectStatus.NotReady:
+                    break;
+
+                case KinectStatus.NotPowered:
+                    if (kinectSensor == null)
+                    {
+                        Clean();
+                        MessageBox.Show("The Kinect needs MORE POWER!");
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Dude I don't know what's wrong. Unhandled Status: " + e.Status);
+                    break;
+            }
+
+        }
+
+        // Constructor
+        private void Initialize()
+        {
+            if (kinectSensor == null)
+                return;
+            kinectSensor.Start();
+        }
+
+        // Destructor
+        private void Clean()
+        {
+            if (kinectSensor != null)
+            {
+                kinectSensor.Stop();
+                kinectSensor = null;
+            }
+        }
+
+    }
+}
